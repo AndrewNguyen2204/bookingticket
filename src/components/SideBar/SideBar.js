4f2363362e204e557f449f5312bea31ed0b06d37
AndrewNguyen2204 <69895099+AndrewNguyen2204@users.noreply.github.com>
2021-05-25 16:06:59 +0700

    reponsive navbar

---
+++ src/components/SideBar/SideBar.js
@@ -0,0 +1,34 @@
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './SideBar.css';
+export default function SideBar(props) {
+
+    const { open, setClose } = props;
+
+    return (
+        <div className={open ? "sidebar-container active" : "sidebar-container"} onClick={() => { setClose(!open) }}>
+
+            <div className="sidebar-content">
+                <div className="close-btn" onClick={() => { setClose(!open) }}><FontAwesomeIcon icon={faTimes} /></div>
+                <ul className="menu">
+                    <li className="flex">
+                        <NavLink to="/home" className="flex items-center -mb-0.5 border-b-2 px-4 border-transparent text-violet-600 border-violet-600">Home</NavLink>
+                    </li>
+                    <li className="flex">
+                        <NavLink to="/contact" className="flex items-center -mb-0.5 border-b-2 px-4 border-transparent text-violet-600 border-violet-600">Contact</NavLink>
+                    </li>
+                    <li className="flex">
+                        <NavLink to="/news" className="flex items-center -mb-0.5 border-b-2 px-4 border-transparent text-violet-600 border-violet-600">News</NavLink>
+                    </li>
+
+                </ul>
+                <div className="items-center flex-shrink-0 flex flex-col">
+                    <button className="self-center px-8 py-3 rounded"><NavLink to="/signin" className="flex items-center -mb-0.5 border-b-2 px-4 border-transparent text-violet-600 border-violet-600">Sign In</NavLink></button>
+                    <button className="self-center px-8 py-3 font-semibold rounded bg-violet-600 text-coolGray-50"><NavLink to="/register" className="flex items-center -mb-0.5 border-b-2 px-4 border-transparent text-violet-600 border-violet-600">Register</NavLink></button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/templates/HomeTemplate/Layout/Header/Header.js
+++ src/templates/HomeTemplate/Layout/Header/Header.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import SideBar from '../../../../components/SideBar/SideBar';
 
 
 export default function Header() {
+
+    const [click, setClick] = useState(false);
+
     return (
         <header className="p-1 lg:p-4 bg-coolGray-100 text-coolGray-800 bg-black bg-opacity-25 text-white fixed z-10 w-full">
             <div className="container flex justify-between h-8 md:h-16 mx-auto">
@@ -28,12 +32,13 @@ export default function Header() {
                     <button className="self-center px-8 py-3 rounded"><NavLink to="/signin" className="flex items-center -mb-0.5 border-b-2 px-4 border-transparent text-violet-600 border-violet-600">Sign In</NavLink></button>
                     <button className="self-center px-8 py-3 font-semibold rounded bg-violet-600 text-coolGray-50"><NavLink to="/register" className="flex items-center -mb-0.5 border-b-2 px-4 border-transparent text-violet-600 border-violet-600">Register</NavLink></button>
                 </div>
-                <button className="p-0 md:p-4 lg:hidden">
+                <button onClick={() => { setClick(!click) }} className="p-0 md:p-4 lg:hidden opacity-60 duration-300 hover:opacity-100">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 text-coolGray-800">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
                 </button>
             </div>
+            <SideBar open={click} setClose={setClick} />
         </header>
 
     )
